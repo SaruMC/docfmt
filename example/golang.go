601a0c5e46082477e00a4977e1ca39e5174f543f
mitsuaaki <alexis.dauge@etu.u-bordeaux.fr>
2024-05-18 22:50:54 +0200

    example for golang

---
+++ example/golang.go
@@ -0,0 +1,16 @@
+// @description Test file for golang documentation
+package main
+
+// @title Golang example for godoc
+// @version 1.0.0
+
+// Add adds two integers together
+// @author mitsuaki
+// @param a int
+// @param b int
+// @return int
+// @complexity O(1)
+// @example Add(1, 2)
+func Add(a, b int) int {
+	return a + b
+}
