eb6891728d17c62fe1d057d61a398b5811a29ef7
mitsuaaki <alexis.dauge@etu.u-bordeaux.fr>
2024-05-18 18:34:41 +0200

    Application testings

--- cmd/godoc/main.go
+++ cmd/godoc/main.go
@@ -1,7 +1,9 @@
-package godoc
+package main
 
 import (
+	"fmt"
 	"github.com/misuaaki/godoc"
+	"github.com/misuaaki/godoc/app/scanner"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
@@ -15,7 +17,22 @@ func main() {
 
 	app.Commands = []*cli.Command{
 		{
-			Name: "",
+			Name:  "scan",
+			Usage: "Scan your project at the given path and under and populate the Packages map - used for testing purposes",
+			Action: func(c *cli.Context) error {
+				path := c.Args().First()
+				if path == "" {
+					return cli.ShowCommandHelp(c, "scan")
+				}
+
+				s := scanner.NewScanner()
+				if err := s.Scan(path, ".go"); err != nil {
+					return err
+				}
+
+				fmt.Println(s.Packages[path].String())
+				return nil
+			},
 		},
 	}
 
